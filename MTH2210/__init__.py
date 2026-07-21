7969cffe508751df8f776bd10275dec9e998ee2d
Antonin Paquette <antonin.paquette-rufiange@polymtl.ca>
2026-07-20 22:21:06 -0400

    - Function spline cubique refactored from MatLab code - Example of a Jupyter notebook for Labo 0

--- MTH2210/__init__.py
+++ MTH2210/__init__.py
@@ -1,11 +1,7 @@
-# from MTH2210.Interpolations.lagrange   import lagrange
-# from MTH2210.Interpolations.spline_cub import spline_cub
-
+from MTH2210.interpolation import lagrange
 from MTH2210.non_linear import bissection, secante, ptfixe, newton1d, newtonNd
-
 from MTH2210.edo import euler, euler_modifie, pt_milieu, rk4
 
-
-__all__ = ["bissection", "secante", "ptfixe", "newton1d", "newtonNd",
-        #    "lagrange", "spline_cub",
+__all__ = ["lagrange",
+           "bissection", "secante", "ptfixe", "newton1d", "newtonNd",
            "euler", "euler_modifie", "pt_milieu", "rk4"]
--- MTH2210/interpolation.py
+++ MTH2210/interpolation.py
@@ -1,77 +1,137 @@
-# function [ Lx ] = lagrange(xi , yi , x)
-# % LAGRANGE	Polyn�me de Lagrange passant par les points xi et yi
-# %
-# % Syntaxe: [ Ly ] = lagrange(xi , yi , x)
-# %
-# % Argument d'entree
-# %	xi		-	Vecteur contenant les abscisses des points d'interpolation
-# %	yi		-	Vecteur contenant les ordonnees des points d'interpolation
-# %	x		-	Vecteur contenant les points o� le polyn�me de Lagrange 
-# %				sera evalue
-# %
-# % Arguments de sortie
-# %	Lx		-	Vecteur contenant les valeurs du polyn�mes aux points x
-# %
-# % Exemples d'appel
-# %	[ Lx ] = lagrange([-1,0,1] , [1,0,1] , linspace(-1,1,200))
-# %
-# % Le calcul du polynome interpolant est base sur la formule barycentrique ;
-# % une variation stable de la formule d'interpolation de Lagrange vue au
-# % cours. Cette implementation est basee sur celle de Greg von Winckel,
-# % elle-meme basee sur l'article de Berrut et Trefethen (SIAM Review, 2004).
-# %
-# % Normalement, on devrait utiliser une version particularisee de cette
-# % fonction si le vecteur xi donne les noeuds d'interpolation de Chebychev.
-# %
-# % "Barycentric interpolation is a variant of Lagrange polynomial
-# % interpolation that is fast and stable. It deserves to be known as the
-# % standard method of polynomial interpolation." (Berrut and Trefethen, 2004).
-# %
-# % Cree par D. Orban, 2009. 
-# % Modifie par A. Paquette-Rufiange, 2018.
-# %
+import numpy as np
+import numpy.typing as npt
+from scipy.sparse import diags_array
+from scipy.sparse.linalg import spsolve
 
-# %% Verification des arguments d'entrees
-# if ~isnumeric(xi) || ~isvector(xi)
-# 	error('Les abscisses xi ne sont pas arrangees en vecteur')
-# elseif ~isnumeric(yi) || ~isvector(yi)
-# 	error('Les ordonnees yi ne sont pas arrangees en vecteur')
-# elseif length(xi) ~= length(yi)
-# 	error('Les vecteurs xi et yi doivent avoir la meme taille');
-# elseif ~isnumeric(x) || ~isvector(x)
-# 	error('Les points o� l''on evalue le polynome doivent etre arrangees en vecteur');
-# elseif isequal(xi,x)
-# 	warning('Le polyn�me d''interpolation est evalue exactement au points d''interpolation')
-# end
+def _init_interpolation(xi, yi, x):
+
+    for arg, arg_name in zip([xi, yi, x],["xi", "yi", "x"]):
+
+        print(xi.__class__)
+
+        try:
+            arg = np.asarray(arg, dtype=np.float64)
+            print(arg.__class__)
+        except:
+            raise Exception(f"Le paramètre {arg_name} ne peut être converti en np.ndarray de type np.float64")
+        
+        if arg.ndim != 1:
+            raise Exception(f"Le paramètre {arg_name} n'est pas unidimensionnelle")
+    
+    xi = np.asarray(xi, dtype=np.float64)
+    yi = np.asarray(yi, dtype=np.float64)
+    x = np.asarray(x, dtype=np.float64)
+
+    if xi.shape[0] != yi.shape[0]:
+        raise Exception("Les paramètres xi et yi doivent être de même taille")
+
+    if xi.shape[0] == x.shape[0] and np.allclose(xi, x):
+        raise Exception("Le polynôme d'interpolation est evalué exactement au points d'interpolation")
+
+    return xi, yi, x
+
+def lagrange(xi:npt.ArrayLike, yi:npt.ArrayLike, x:npt.ArrayLike) -> npt.NDArray[np.float64]:
+    """
+    Polynôme de Lagrange passant par les points xi et yi
+
+    Le calcul du polynome interpolant est base sur la formule barycentrique 
+    une variation stable de la formule d'interpolation de Lagrange vue au
+    cours. Cette implementation est basee sur celle de Greg von Winckel,
+    elle-meme basee sur l'article de Berrut et Trefethen [1]_.
 
 
-# %% Calcul de l'interpolant aux points x
+    Parameters
+    ----------
+    xi : Arraylike de dimension 1
+        Abscisses des points d'interpolation
+    yi : Arraylike de dimension 1
+        Ordonnées des points d'interpolation
+    x : Arraylike de dimension 1
+        Points où le polynôme de Lagrange sera évalué
 
-# M = length(xi);  xxi = reshape(xi,M,1);  % Donnees en vecteur colonne
-# N = length(x);   xx = reshape(x,N,1);    % Grille fine en vecteur colonne
+    Returns
+    -------
+    Lx : NDArray de dimension 1
+        Valeur du polynôme de Lagrange aux points x
 
-# Xi = repmat(xxi,1,M);
-# W = repmat(1./prod(Xi-Xi.'+eye(M),1),N,1);   % Poids barycentriques
-# xdist=repmat(xx,1,M)-repmat(xxi.',N,1);   % Distances aux points d'interpolation
+    See Also
+    --------
+    splinec : Spline cubique
 
-# % On place des NaN aux noeuds d'interpolation pour eviter la division par zero
-# [fixi,fixj]=find(xdist==0);
-# xdist(fixi,fixj)=NaN;
-# H=W./xdist;
-
-# Lx=(H * reshape(yi,M,1))./sum(H,2);  % Valeurs du polynome sur la grille fine
-# Lx(fixi)=yi(fixj);        % On restaure les valeurs exacte a la place des NaN
-# if (size(Lx) ~= size(x))
-#   Lx = Lx';
-# end
+    Examples
+    --------
 
 
-# function [ Sx ] = splinec( xi , yi , x , type_S , val_S)
-# % SPLINEC	Spline cubique passant par les points xi et yi avec differents 
-# %			type de conditions frontieres
-# %
-# % Syntaxe: [ Sx ] = splinec( xi , yi , x , type_f , val_f)
-# %
+    References
+    ----------
+    .. [1] Barycentric interpolation is a variant of Lagrange polynomial interpolation that is fast and stable. It deserves to be known as the standard method of polynomial interpolation." (Berrut and Trefethen, 2004)
+    """
+    (xi , yi , x) = _init_interpolation(xi, yi, x)
+ 
+    M = xi.shape[0]  
+    N = x.shape[0]   
+
+    # Calcul des poids barycentriques
+    Xi = np.tile(xi, (M,1)) 
+    W = 1/np.prod(Xi - Xi.T + np.eye(M), axis=0)
+    W_repeated = np.tile(W, (N,1))
+
+    # Calcul des distances entre les xi et x
+    xdist = np.tile(x, (M,1)).T - np.tile(xi, (N,1))
+
+    # Change the null distance by np.nan if x xontains xi, so that the distance is null and the inverse should be identified by np.nan 
+    idx_0 = np.argwhere(np.isclose(xdist, 0))
+    xdist[idx_0[:,0], idx_0[:,1]] = np.nan
+
+    H = W_repeated/xdist
+    Lx = np.linalg.matmul(H, yi) / np.sum(H, axis=1)
+
+    Lx[idx_0[:,0]] = yi[idx_0[:,1]] 
+
+    return Lx
+
+def _diff_div(x,y,n):
+    diff_x	=	x[n:] - x[0:-n]
+    diff_y	=	np.diff(y)
+    f		=	diff_y/diff_x
+
+    return f
+
+
+def _poly_spline(xi,yi,Spp,h,x):
+
+    Px = -Spp[0] / (6*h) * (x - xi[1])**3 + Spp[1] / (6*h) * (x - xi[0])**3 - \
+          (yi[0] / h - Spp[0]*h/6) * (x - xi[1]) + (yi[1]/h - Spp[1]*h/6) * (x - xi[0])
+    
+    return Px
+
+def splinec(xi:npt.ArrayLike, yi:npt.ArrayLike, x:npt.ArrayLike, type_S:npt.ArrayLike, val_S:npt.ArrayLike) -> npt.NDArray[np.float64]:
+    """
+    Spline cubique passant par les points xi et yi avec differents 
+    type de conditions frontieres
+
+    Parameters
+    ----------
+    xi : Arraylike de dimension 1
+        Abscisses des points d'interpolation
+    yi : Arraylike de dimension 1
+        Ordonnées des points d'interpolation
+    x : Arraylike de dimension 1
+        Points où le polynôme de Lagrange sera évalué
+
+    Returns
+    -------
+    Xx : NDArray de dimension 1
+        Valeur du polynôme de Lagrange aux points x
+
+    See Also
+    --------
+    lagrange : Interpolant de Lagrange
+
+    Examples
+    --------
+
+
 # % Argument d'entree
 # %	xi		-	Vecteur contenant les abscisses des points d'interpolation
 # %	yi		-	Vecteur contenant les ordonnees des points d'interpolation
@@ -104,20 +164,10 @@
 # %	[ Sx ] = splinec([1,2,4,5], [1,9,2,11], linspace(1,5), [4,4] , [-30,-10])
 # %	[ Sx ] = splinec([1,2,4,5], [1,9,2,11], linspace(1,5), [3,4] , [nan,-10])
 
+    """
 
-# %% Verification des arguments d'entrees
-# if ~isnumeric(xi) || ~isvector(xi)
-# 	error('Les abscisses xi ne sont pas arrangees en vecteur')
-# elseif ~isequal(sort(xi),xi)
-# 	error('Les abscisses ne sont pas arrangees en ordre croissant')
-# elseif ~isnumeric(yi) || ~isvector(yi)
-# 	error('Les ordonnees yi ne sont pas arrangees en vecteur')
-# elseif length(xi) ~= length(yi)
-# 	error('Les vecteurs xi et yi doivent avoir la meme taille');
-# elseif ~isnumeric(x) || ~isvector(x)
-# 	error('Les points o� l''on evalue la spline cubique doivent etre arrangees en vecteur');
-# elseif isequal(xi,x)
-# 	warning('Le polyn�me d''interpolation est evalue exactement au points d''interpolation')
+    (xi , yi , x) = _init_interpolation(xi, yi, x)
+    
 # elseif ~isnumeric(type_S) || ~isvector(type_S) || length(type_S)~=2 || ~any(type_S(1)==[1,2,3,4]) || ~any(type_S(2)==[1,2,3,4])
 # 	error('Les types de conditions frontieres ne sont pas valide')
 # elseif ~isnumeric(val_S) || ~isvector(val_S) || length(val_S)~=2
@@ -125,79 +175,71 @@
 # end
 
 
-# %% Calcul des coefficients S''
+    #  Calcul des coefficients S''
 
-# % Assemblage de la matrice
-# nb_f	=	length(xi);
-# h		=	reshape(diff(xi),1,[]);
-# denom	=	h(1:end-1)+h(2:end);
+    # Assemblage de la matrice
+    nb_f	=	xi.shape[0]
+    h		=	np.diff(xi)
+    denom	=	h[0:-1] + h[1:]
 
-# M			=	[[h(1:end-1)./denom,0,0]',[0;2*ones(nb_f-2,1);0],[0,0,h(2:end)./denom]'];
-# matrice		=	spdiags(M,[-1,0,1],nb_f,nb_f);
+    diagonals = [np.concatenate([h[0:-1]/denom,[0]]), 
+                 np.concatenate([[0], 2*np.ones(nb_f-2), [0]]),
+                 np.concatenate([[0], h[1:]/denom])]
+    
+    # Création de la matrice tridiagonale sparse
+    matrice_temp = diags_array(diagonals, offsets=[-1,0,1])
+    
+    # Changement de l'encodage sparse pour pouvoir effectuer des assignements
+    matrice = matrice_temp.tolil()
 
-# % Calcul des 2eme differences divisees
-# mat_diff_div1	=	diff_div(xi,yi,1);
-# mat_diff_div2	=	diff_div(xi,mat_diff_div1,2);
+    # Calcul des 2eme differences divisees
+    mat_diff_div1 =	_diff_div(xi, yi, 1)
+    mat_diff_div2 =	_diff_div(xi, mat_diff_div1, 2)
 
-# % Terme de droite
-# B	=	zeros(nb_f,1);
-# B(2:end-1)	=	6*mat_diff_div2;
+    # Terme de droite
+    B = np.zeros(nb_f)
+    B[1:-1]	= 6*mat_diff_div2
 
-# % Imposition des conditions frontieres
-# switch type_S(1)
-# 	case 1
-# 		matrice(1,1)		=	1;
-# 		B(1)				=	0;
-# 	case 2
-# 		matrice(1,1)		=	1;
-# 		B(1)				=	val_S(1);
-# 	case 3
-# 		matrice(1,[1,2])		=	[1,-1];
-# 		B(1)					=	0;
-# 	case 4
-# 		matrice(1,[1,2])		=	[2,1];
-# 		B(1)					=	6/h(1)   * ((yi(2) - yi(1))/h(1) - val_S(1));
-# end
+    # Imposition des conditions frontieres
+    match type_S[0]:
+        case 1:
+            matrice[0,0] = 1
+            B[0]         = 0
+        case 2:
+            matrice[0,0] = 1
+            B[0]         = val_S[0]
+        case 3:
+            matrice[0,0:2] = [1,-1]
+            B[0]           = 0
+        case 4:
+            matrice[0,0:2] = [2,1]
+            B[0]           = 6/h[0] * ((yi[1] - yi[0])/h[0] - val_S[0])
 
-# switch type_S(2)
-# 	case 1
-# 		matrice(nb_f,nb_f)	=	1;
-# 		B(end)				=	0;
-# 	case 2
-# 		matrice(nb_f,nb_f)	=	1;
-# 		B(end)				=	val_S(2);
-# 	case 3
-# 		matrice(end,[end-1,end])=	[-1,1];
-# 		B(end)					=	0;
-# 	case 4
-# 		matrice(end,[end-1,end])=	[1,2];
-# 		B(end)					=	6/h(end) * (val_S(2) - (yi(end) - yi(end-1))/h(end));
-# end
+    match type_S[1]:
+        case 1:
+            matrice[-1,-1] = 1
+            B[-1]          = 0 
+        case 2:
+            matrice[-1,-1] = 1
+            B[-1]		   = val_S[1]
+        case 3:
+            matrice[-1,-2:] = [-1,1]
+            B[-1]   		= 0
+        case 4:
+            matrice[-1,-2:] = [1,2] 
+            B[-1]			= 6/h[-1] * (val_S[1] - (yi[-1] - yi[-2])/h[-1])
 
-# % Resolution du systeme lineaire
-# Spp		=	matrice\B;
-
-
-# %% Calcul de la spline aux points x 
-
-# Sx	=	nan(size(x));
-
-# for t=1:nb_f-1
-# 	x_inter		=	(x>= xi(t)) & (x <= xi(t+1));
-# 	Sx(x_inter) =	poly_spline(xi(t:t+1),yi(t:t+1),Spp(t:t+1),h(t),x(x_inter));
-# end
-
-# end
+    # Resolution du systeme lineaire avec reformattage de la matrice sparse
+    Spp	= spsolve(matrice.todia(), B)
 
 
-# function [ f ] = diff_div(x,y,n)
-# 	diff_x	=	reshape(x(n+1:end) - x(1:end-n),1,[]);
-# 	diff_y	=	reshape(diff(y),1,[]);
-# 	f		=	diff_y./diff_x;
-# end
+    # Calcul de la spline aux points x 
 
+    Sx	=	np.nan * np.ones(x.shape[0])
 
-# function [ Px ] = poly_spline(xi,yi,Spp,h,x)
-# 	Px	=	-Spp(1)/(6*h)*(x-xi(2)).^3 + Spp(2)/(6*h)*(x-xi(1)).^3 - ...
-# 			 (yi(1)/h - Spp(1)*h/6)*(x-xi(2)) + (yi(2)/h - Spp(2)*h/6)*(x-xi(1));
-# end
+    for t in range(nb_f-1):
+        x_inter	    = (x>= xi[t]) & (x <= xi[t+1])
+        Sx[x_inter] = _poly_spline(xi[t:t+2], yi[t:t+2],Spp[t:t+2], h[t], x[x_inter])
+
+    return Sx
+
